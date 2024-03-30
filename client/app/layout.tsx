4b5bd5fbcb2bc4455ee4b8c3da3346f1985ddce6
tejartr7 <raghusai126@gmail.com>
2024-03-30 20:38:41 +0530

    fixing provider issue

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <Providers>
-      <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
+      <Providers>
         <div>
           <body>
             <main id="root">
@@ -30,7 +30,7 @@ export default function RootLayout({
             </main>
           </body>
         </div>
-      </html>
-    </Providers>
+      </Providers>
+    </html>
   );
 }

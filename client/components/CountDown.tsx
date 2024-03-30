6e540ddedeb3d61ee9127d1f3aa2c1f7370cd9c4
tejartr7 <raghusai126@gmail.com>
2024-03-30 19:51:37 +0530

    fixed the use-react-screenshot issue

--- client/components/CountDown.tsx
+++ client/components/CountDown.tsx
@@ -1,26 +1,40 @@
 'use client'
-import React, { useEffect } from 'react';
-import { useSystem } from '@/hooks/use-system';
+import React, { useEffect, useState } from "react";
+import { useSystem } from "@/hooks/use-system";
 
 const Countdown = () => {
   const { counter } = useSystem();
+  const [remainingTime, setRemainingTime] = useState(counter);
 
-  const formatedCountdown = {
-    minutes: Math.floor(counter / 60000),
-    seconds: Math.floor((counter % 60000) / 1000),
+  // Update remainingTime every second
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (remainingTime > 0) {
+        setRemainingTime((prevTime) => prevTime - 1000);
+      }
+    }, 1000);
+
+    // Clean up interval
+    return () => clearInterval(intervalId);
+  }, [counter, remainingTime]); // Re-run effect when counter changes or remainingTime changes
+
+  // Calculate formatted countdown
+  const formattedCountdown = {
+    minutes: Math.floor(remainingTime / 60000),
+    seconds: Math.floor((remainingTime % 60000) / 1000),
   };
 
   return (
-    <div className='flex justify-end'>
-      <div className='rounded-lg p-3'>
-        <span className='text-right font-mono text-lg lg:text-xl'>
-          {formatedCountdown.minutes < 10
-            ? `0${formatedCountdown.minutes}`
-            : formatedCountdown.minutes}
+    <div className="flex justify-end">
+      <div className="rounded-lg p-3">
+        <span className="text-right font-mono text-lg lg:text-xl">
+          {formattedCountdown.minutes < 10
+            ? `0${formattedCountdown.minutes}`
+            : formattedCountdown.minutes}
           :
-          {formatedCountdown.seconds < 10
-            ? `0${formatedCountdown.seconds}`
-            : formatedCountdown.seconds}
+          {formattedCountdown.seconds < 10
+            ? `0${formattedCountdown.seconds}`
+            : formattedCountdown.seconds}
         </span>
       </div>
     </div>

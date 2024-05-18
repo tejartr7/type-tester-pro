3edf3c8d87d8f199c5432f22a0c37239c3ca426a
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-05-18 17:10:35 +0530

    fixing the screen issues

--- client/components/WordWrapper.tsx
+++ client/components/WordWrapper.tsx
@@ -1,4 +1,5 @@
-import { MdCenterFocusStrong } from 'react-icons/md';
+import { MdCenterFocusStrong } from "react-icons/md";
+import { useState, useEffect } from "react";
 
 type WordWrapperProps = {
   children: React.ReactNode;
@@ -7,28 +8,53 @@ type WordWrapperProps = {
 };
 
 const WordWrapper = ({ children, focused, setFocused }: WordWrapperProps) => {
+  const [isSmallDevice, setIsSmallDevice] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallDevice(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <div
-        className={`${
-          focused ? 'opacity-0' : 'opacity-100'
-        } flex items-center justify-center gap-3 transition-all duration-500 `}
-      >
-        <MdCenterFocusStrong className='text-center text-2xl' />
-        <span className={`text-center text-lg `}>
-          Focus to start typing
-        </span>
-      </div>
-      <div
-        className={`relative mt-5 flex justify-center focus:border-0 focus:border-none focus:outline-none ${
-          focused ? 'blur-none' : 'cursor-pointer blur-md'
-        } `}
-        tabIndex={0}
-        onFocus={() => setFocused(true)}
-        onBlur={() => setFocused(false)}
-      >
-        {children}
-      </div>
+      {isSmallDevice ? (
+        <div className="text-center mt-5">
+          <p>
+            Type Tester Pro is not supported on small devices as their keyboards
+            have auto-suggest options.
+          </p>
+        </div>
+      ) : (
+        <>
+          <div
+            className={`${
+              focused ? "opacity-0" : "opacity-100"
+            } flex items-center justify-center gap-3 transition-all duration-500 `}
+          >
+            <MdCenterFocusStrong className="text-center text-2xl" />
+            <span className={`text-center text-lg `}>
+              Focus to start typing
+            </span>
+          </div>
+          <div
+            className={`relative mt-5 flex justify-center focus:border-0 focus:border-none focus:outline-none ${
+              focused ? "blur-none" : "cursor-pointer blur-md"
+            } `}
+            tabIndex={0}
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
+          >
+            {children}
+          </div>
+        </>
+      )}
     </>
   );
 };

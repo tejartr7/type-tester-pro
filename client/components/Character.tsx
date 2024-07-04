9e859e5e716c737430ed0c233a8eabbe708b6b8c
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-07-04 16:09:09 +0530

    ui fixed

--- client/components/Character.tsx
+++ client/components/Character.tsx
@@ -10,6 +10,7 @@ const Character = ({ state, character, isCrossedOut }: CharactersProps) => {
   return (
     <span
       className={`${state === undefined ? '' : state ? 'text-green-500' : 'text-red-500'}`}
+      style={{backgroundColor: state === false && character === ' ' ? '#BD0101' : '',}}
     >
       {character}
     </span>
